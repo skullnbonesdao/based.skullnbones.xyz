b12ea140e642aa0436404582d94347437fcedab5
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2025-01-07 22:09:40 +0000

    deploy: 366c092e7f72e18581f6d1c772ec9e5832f2de8c

---
+++ assets/index-BOC1rCrQ.js
@@ -0,0 +1,1 @@
+import{aW as r}from"./index-D5e0EpkC.js";import{r as o}from"./index-DVKssMzM.js";var s=o();const e=r(s);export{e as b};
---
+++ assets/notify-CYUyPf4U.js
@@ -0,0 +1,1 @@
+import{N as o}from"./index-D5e0EpkC.js";o.setDefaults({position:"bottom-right",timeout:2500,textColor:"white",actions:[{icon:"close",color:"white"}]});console.log("[booted] Notofy");
