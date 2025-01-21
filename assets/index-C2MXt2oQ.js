5b0cf6e735377e97b651b450c94235a97b408e57
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2025-01-21 22:09:38 +0000

    deploy: eac428761d8c3a465a9abf6f0ec34f4a5174c2d3

---
+++ assets/index-C2MXt2oQ.js
@@ -0,0 +1,1 @@
+import{bK as r}from"./index-COgUWfzO.js";import{r as o}from"./index-DVKssMzM.js";var s=o();const e=r(s);export{e as b};
--- assets/notify-Djcj134N.js
+++
@@ -1,1 +0,0 @@
-import{N as o}from"./index-BBkArlIb.js";o.setDefaults({position:"bottom-right",timeout:2500,textColor:"white",actions:[{icon:"close",color:"white"}]});console.log("[added] Plugin: Notify");
