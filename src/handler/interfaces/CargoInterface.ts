82bb5ad80d5e9cafd9f37e8acb1169803ad8be68
SnB <skullandbonesdao@gmail.com>
2025-04-11 23:53:53 +0200

    cargo pod now computed and not static

--- src/handler/interfaces/CargoInterface.ts
+++ src/handler/interfaces/CargoInterface.ts
@@ -1,6 +1,10 @@
 import { PublicKey } from '@solana/web3.js'
 import { useWorkspaceAdapter } from 'src/handler/connector'
 import { CargoType } from '@staratlas/cargo'
+import { getCargoPodsByAuthority } from '@staratlas/sage'
+import { useRPCStore } from 'stores/rpcStore'
+import { findStarbasePlayerAddress } from 'src/handler/interfaces/GameInterface'
+import { BN } from '@staratlas/anchor'
 
 export function findCargoTypeAddress(cargoStatsDefinition: PublicKey, mint: PublicKey) {
   return CargoType.findAddress(
@@ -12,24 +16,19 @@ export function findCargoTypeAddress(cargoStatsDefinition: PublicKey, mint: Publ
 }
 
 export async function findCargoPodAddress(index: number = 0) {
-  /*
-    const cargoPods = await getCargoPodsByAuthority(
-      useRPCStore().connection,
-      useWorkspaceAdapter()!.cargoProgram.value!,
-      findStarbasePlayerAddress(),
-    )
-  */
+  const cargoPods = await getCargoPodsByAuthority(
+    useRPCStore().connection,
+    useWorkspaceAdapter()!.cargoProgram.value!,
+    findStarbasePlayerAddress(),
+  )
 
-  /*
-    cargoPods.forEach((cargoPod) => {
-      console.log(cargoPod.key.toString())
-    })
-  */
+  const cargoPodAddress = cargoPods
+    .filter((cargoPods) => cargoPods.type == 'ok')
+    .find((cargoPod) => {
+      const temp = new BN(cargoPod.data.stats.toString())
+      return temp.gt(new BN(0))
+    })?.key
 
-  return new PublicKey('EatffFvG5sKh6qzoyZKtJ9JGCRpYbwWQpfQGta6c9cRG') //
-  //return new PublicKey('3GJB59zhLisRVKe8gLYDSWSU8otuZWZrw8NGLhWuYKAm') //
-  //return new PublicKey('6uJuzcGKXiFfCXPkrAx9xzgfUAn8LoAD9ieTs43Um3H9') //
-  //return new PublicKey('8YPDojQ5dVz6QnpDHQXYUquYa5qcjhPG6nVAG8bqUDek') //
-
-  //else throw Error('Unable to find cargo POD')
+  if (cargoPodAddress) return cargoPodAddress
+  else throw Error('Unable to find cargo POD')
 }
---
+++ src/pages/TestPage.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { findCargoPodAddress } from 'src/handler/interfaces/CargoInterface'
+
+const data = ref()
+onMounted(async () => {
+  data.value = await findCargoPodAddress()
+})
+</script>
+
+<template>
+  <q-page>
+    <div>TEST PAGE</div>
+    {{ data }}
+  </q-page>
+</template>
+
+<style scoped></style>
